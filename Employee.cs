8ec93be005b909d4aabdd18df09fe95edfe1a925
ondickson <owusudicksonnyantakyi@gmail.com>
2024-10-22 12:28:16 +0800

    initial commit

---
+++ Employee.cs
@@ -0,0 +1,67 @@
+﻿namespace E41Owusu
+{
+    public class Employee
+    {
+        // Attribute declaration
+        private int id;
+        private string firstname;
+        private string lastname;
+        private string newname;
+        private double salary;
+        private double percentage;
+        private double percentageincrease;
+
+        // Default constructor
+        public Employee()
+        {
+            id = 0;
+            firstname = "";
+            lastname = "";
+            newname = "";
+            salary = 0;
+            percentage = 0;
+            percentageincrease = 0;
+        }
+
+        // Parameterized constructor
+        public Employee(int id, string firstname, string lastname, string newname, double salary, double percentage, double percentageincrease)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.newname= newname;
+            this.salary = salary;
+            this.percentage = percentage;
+            this.percentageincrease = percentageincrease;
+        }
+
+        // Mutator methods (Setters)
+        public void setId(int id) { this.id = id; }
+        public void setFirstName(string firstname) { this.firstname = firstname; }
+        public void setLastName(string lastname) { this.lastname = lastname; }
+        public void setNewName(string newname) { this.newname = newname; }
+        public void setSalary(double salary) { this.salary = salary; }
+        public void setPercentage(double percentage) { this.percentage = percentage; }
+        public void setPercentageIncrease(double percentageincrease) { this.percentageincrease = percentageincrease; }
+
+        // Accessor methods (Getters)
+        public int getId() { return id; }
+        public string getFirstName() { return firstname; }
+        public string getLastName() { return lastname; }
+        public string getNewName() { return newname; }
+        public double getSalary() { return salary; }
+        public double getPercentage() { return percentage; }
+        public double getPercentageIncrease() { return percentageincrease; }
+
+        // Method to calculate annual salary
+        public double getAnnualSalary()
+        {
+            return salary * 12;
+        }
+
+        public double getNewPercentageIncrease()
+        {
+            return salary + ((percentage/100) * salary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using E41Owusu;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // instantiate an object of the Employee class
+        Employee employee = new Employee();
+        LinkedList<int> a = new LinkedList<int>();
+
+
+        Console.Write("Enter firstname: ");
+        employee.setFirstName(Console.ReadLine());
+
+        Console.Write("Enter lastname: ");
+        employee.setLastName(Console.ReadLine());
+
+        Console.Write("Enter salary: ");
+        employee.setSalary(double.Parse(Console.ReadLine()));
+
+        // Loop to continue displaying options
+        bool continueProgram = true;
+
+        while (continueProgram)
+        {
+            // Display the menu options
+            DisplayMenu();
+
+            // Read user input
+            Console.Write("\nEnter action: ");
+            string input = Console.ReadLine();
+
+            // Validate and convert input to an integer
+            if (int.TryParse(input, out int option))
+            {
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Name: " + employee.getFirstName() + " " + employee.getLastName());
+                        Console.WriteLine("Current Salary is " + employee.getSalary());
+                        Console.WriteLine("Annual Salary is " + employee.getAnnualSalary());
+                        break;
+                    case 2:
+                        Console.Write("Enter increase in percent(%): ");
+                        employee.setPercentage(double.Parse(Console.ReadLine()));
+                        Console.WriteLine("New salary is " + employee.getNewPercentageIncrease());
+                        break;
+                    case 3:
+                        // Enter details for a new employee
+                        Console.WriteLine("\nEntering new employee information...");
+                        employee = new Employee();  // Reinitialize the Employee object
+                        Console.Write("Enter first name: ");
+                        employee.setFirstName(Console.ReadLine());
+
+                        Console.Write("Enter last name: ");
+                        employee.setLastName(Console.ReadLine());
+
+                        Console.Write("Enter salary: ");
+                        employee.setSalary(double.Parse(Console.ReadLine()));
+                        break;
+                    case 4:
+                        Console.WriteLine("Exit");
+                        continueProgram = false; 
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please choose a number between 1 and 4.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+
+    // Function to display the menu options
+    static void DisplayMenu()
+    {
+        Console.WriteLine("\n[1] Display Annual Salary");
+        Console.WriteLine("[2] Raise Salary");
+        Console.WriteLine("[3] Enter Another Employee");
+        Console.WriteLine("[4] Exit");
+    }
+
+}
